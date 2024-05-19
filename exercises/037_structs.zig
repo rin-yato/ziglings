552cb7d98f4029f4360c3f096f82f1efe9125170
rin-yato <chearithorn@gmail.com>
2024-05-19 16:53:40 +0700

    31-40

--- exercises/037_structs.zig
+++ exercises/037_structs.zig
@@ -33,18 +33,15 @@ const Role = enum {
 // Please add a new property to this struct called "health" and make
 // it a u8 integer type.
 const Character = struct {
-    role: Role,
     gold: u32,
+    role: Role,
+    health: u8,
     experience: u32,
 };
 
 pub fn main() void {
     // Please initialize Glorp with 100 health.
-    var glorp_the_wise = Character{
-        .role = Role.wizard,
-        .gold = 20,
-        .experience = 10,
-    };
+    var glorp_the_wise = Character{ .role = Role.wizard, .gold = 20, .experience = 10, .health = 100 };
 
     // Glorp gains some gold.
     glorp_the_wise.gold += 5;
